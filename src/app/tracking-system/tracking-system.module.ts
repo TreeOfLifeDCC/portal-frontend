476153c8426b7f5cfecbd8cc3e8c206e93eaf3d1
Alexey Sokolov <alexey@ebi.ac.uk>
2020-09-01 14:53:42 +0100

    Adding data to tracking system

--- src/app/tracking-system/tracking-system.module.ts
+++ src/app/tracking-system/tracking-system.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrackingSystemComponent } from './tracking-system/tracking-system.component';
 import {TrackingSystemRoutingModule} from './tracking-system-routing.module';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 
@@ -9,7 +16,14 @@ import {TrackingSystemRoutingModule} from './tracking-system-routing.module';
   declarations: [TrackingSystemComponent],
   imports: [
     CommonModule,
-      TrackingSystemRoutingModule
+    TrackingSystemRoutingModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSortModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatPaginatorModule
   ]
 })
 export class TrackingSystemModule { }
--- src/app/tracking-system/tracking-system/tracking-system.component.ts
+++ src/app/tracking-system/tracking-system/tracking-system.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { Sample, samples } from '../model/tracking-system.model';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-tracking-system',
@@ -6,10 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tracking-system.component.css']
 })
 export class TrackingSystemComponent implements OnInit {
+  displayedColumns = ['Organism', 'Common name', 'Metadata submitted to BioSamples',
+    'Raw data submitted to ENA', 'Mapped reads submitted to ENA', 'Assemblies submitted to ENA',
+    'Annotation submitted to ENA'];
+  dataSource = new MatTableDataSource<Sample>(samples);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort, {}) sort: MatSort;
 
-  constructor() { }
+  constructor(private titleService: Title) { }
 
   ngOnInit(): void {
+    this.titleService.setTitle('Tracking system');
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  filterProduct(value: string): void {
+    this.dataSource.filter = value.trim().toLowerCase();
   }
 
 }
