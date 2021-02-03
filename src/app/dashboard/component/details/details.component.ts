25723320302c49347541655d6a720f36a0c680d5
ktibrahim <ibrahim@ebi.ac.uk>
2021-02-03 12:31:46 +0000

    DCP-2790

--- src/app/dashboard/component/details/details.component.ts
+++ src/app/dashboard/component/details/details.component.ts
@@ -17,14 +17,17 @@ export class DetailsComponent implements OnInit {
   bioSampleId;
   bioSampleObj;
   dataSourceFiles;
+  dataSourceFilesCount;
   dataSourceAssemblies;
+  dataSourceAssembliesCount;
 
   displayedColumnsFiles = ['study_accession', 'experiment_accession', 'run_accession', 'fastq_ftp', 'submitted_ftp',
-      'instrument_platform', 'instrument_model', 'library_layout', 'library_strategy', 'library_source',
-      'library_selection'];
+    'instrument_platform', 'instrument_model', 'library_layout', 'library_strategy', 'library_source',
+    'library_selection'];
   displayedColumnsAssemblies = ['accession', 'assembly_name', 'description', 'version'];
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  @ViewChild('experimentsTable') paginator: MatPaginator;
+  @ViewChild('assembliesTable') asPaginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(private route: ActivatedRoute, private dashboardService: DashboardService) {
     this.route.params.subscribe(param => this.bioSampleId = param.id);
@@ -39,20 +42,35 @@ export class DetailsComponent implements OnInit {
     this.dashboardService.getBiosampleByAccession(this.bioSampleId)
       .subscribe(
         data => {
+          const unpackedData = [];
           this.bioSampleObj = data;
-          this.dataSourceFiles = new MatTableDataSource<Sample>(this.bioSampleObj.experiment);
-          this.dataSourceAssemblies = new MatTableDataSource<any>(this.bioSampleObj.assemblies);
-          this.dataSourceFiles.paginator = this.paginator;
-          this.dataSourceFiles.sort = this.sort;
+          setTimeout(() => {
+            this.dataSourceFiles = new MatTableDataSource<Sample>(this.bioSampleObj.experiment);
+            this.dataSourceFilesCount = this.bioSampleObj.experiment.length;
+            this.dataSourceAssemblies = new MatTableDataSource<any>(this.bioSampleObj.assemblies);
+            this.dataSourceAssembliesCount = this.bioSampleObj.assemblies.length;
+
+            this.dataSourceFiles.paginator = this.paginator;
+            this.dataSourceFiles.sort = this.sort;
+
+            this.dataSourceAssemblies.paginator = this.asPaginator;
+            this.dataSourceAssemblies.sort = this.sort;
+          }, 50)
+
         },
         err => console.log(err)
       );
   }
 
-    // tslint:disable-next-line:typedef
-    applyFilter(event: Event) {
-        const filterValue = (event.target as HTMLInputElement).value;
-        this.dataSourceFiles.filter = filterValue.trim().toLowerCase();
-    }
+  // tslint:disable-next-line:typedef
+  filesSearch(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceFiles.filter = filterValue.trim().toLowerCase();
+  }
+
+  assembliesSearch(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceAssemblies.filter = filterValue.trim().toLowerCase();
+  }
 
 }
