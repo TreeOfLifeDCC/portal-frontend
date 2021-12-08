ccedb86f9dddbf11210535587d6fd4896b2d4384
Raheela <32760007+Raheela1024@users.noreply.github.com>
2021-12-08 13:14:35 +0000

    linear node functionality

--- src/assets/js/d3tree.js
+++ src/assets/js/d3tree.js
@@ -245,8 +245,15 @@ treeJSON = d3.json(url, function(error, treeData) {
 
     function click(d) {
         clickCount++;
+        if ((d.children && d.children.length === 1) || (d._children && d._children.length === 1)) {
+            singleClickTimer = setTimeout(function () {
+                clickCount = 0;
+                expand(d);
+                singleClick(d);
+            }, 400);
+        }
         if (clickCount === 1) {
-            singleClickTimer = setTimeout(function() {
+            singleClickTimer = setTimeout(function () {
                 clickCount = 0;
                 singleClick(d);
             }, 400);
