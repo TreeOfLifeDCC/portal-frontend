b72c807342e366dadfd15b73f1a7cbe560be4891
ktibrahim <ibrahim@ebi.ac.uk>
2022-01-18 17:37:48 +0000

    tolid and INSDC_ID added to data table and details page

--- src/app/dashboard/component/dashboard.component.ts
+++ src/app/dashboard/component/dashboard.component.ts
@@ -102,7 +102,7 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   itemLimitBiosampleFilter: number;
   itemLimitEnaFilter: number;
 
-  dataColumnsDefination = [{name: "Organism", column: "organism", selected: true},{name: "Common Name", column: "commonName", selected: true},{name: "Current Status", column: "currentStatus", selected: true},{name: "External references", column: "goatInfo", selected: true},{name: "Submitted to Biosamples", column: "biosamples", selected: false},{name: "Raw data submitted to ENA", column: "raw_data", selected: false},{name: "Mapped reads submitted to ENA", column: "mapped_reads", selected: false},{name: "Assemblies submitted to ENA", column: "assemblies", selected: false},{name: "Annotation complete", column: "annotation_complete", selected: false}, {name: "Annotation submitted to ENA", column: "annotation", selected: false}]
+  dataColumnsDefination = [{name: "Organism", column: "organism", selected: true},{name: "ToL ID", column: "tolid", selected: true},{name: "INSDC ID", column: "INSDC_ID", selected: true},{name: "Common Name", column: "commonName", selected: true},{name: "Current Status", column: "currentStatus", selected: true},{name: "External references", column: "goatInfo", selected: true},{name: "Submitted to Biosamples", column: "biosamples", selected: false},{name: "Raw data submitted to ENA", column: "raw_data", selected: false},{name: "Mapped reads submitted to ENA", column: "mapped_reads", selected: false},{name: "Assemblies submitted to ENA", column: "assemblies", selected: false},{name: "Annotation complete", column: "annotation_complete", selected: false}, {name: "Annotation submitted to ENA", column: "annotation", selected: false}]
   displayedColumns = [];
   constructor(private titleService: Title, private dashboardService: DashboardService,
     private activatedRoute: ActivatedRoute, private router: Router, private spinner: NgxSpinnerService, private taxanomyService: TaxanomyService) { }
@@ -352,6 +352,10 @@ export class DashboardComponent implements OnInit, AfterViewInit {
       data = data._source;
     }
     for (const key of Object.keys(data)) {
+      if(key === 'experiment') {
+        let exp = data[key]
+        dataToReturn['INSDC_ID'] = exp[0].study_accession;
+      }
       if(key === 'tax_id') {
         dataToReturn['goatInfo'] = "https://goat.genomehubs.org/records?record_id="+data[key]+"&result=taxon&taxonomy=ncbi#"+dataToReturn["organism"]
         dataToReturn[key] = data[key];
