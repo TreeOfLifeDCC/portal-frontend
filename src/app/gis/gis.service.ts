91db920834395351d108d0f28d15456b40f37f3f
Raheela <32760007+Raheela1024@users.noreply.github.com>
2022-07-14 14:23:10 +0100

    gis changes reverted.

--- src/app/gis/gis.service.ts
+++ src/app/gis/gis.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
 })
 export class GisService {
 
-  private API_BASE_URL = 'https://portal.darwintreeoflife.org/api';
-  // private API_BASE_URL = 'http://localhost:8080';
+  // private API_BASE_URL = 'https://portal.darwintreeoflife.org/api';
+  private API_BASE_URL = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
 
