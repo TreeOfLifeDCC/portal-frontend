5cf389b7d36025365ef238e2c9a6f0e43808c57d
yroochun <yroochun@ebi.ac.uk>
2025-02-25 13:44:51 +0000

    fixed and refactored publications component

--- src/app/publications/publications.component.ts
+++ src/app/publications/publications.component.ts
@@ -3,7 +3,7 @@ import {MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatH
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort, MatSortHeader} from '@angular/material/sort';
 import {Title} from '@angular/platform-browser';
-import {NgxSpinnerService} from 'ngx-spinner';
+import {NgxSpinnerModule, NgxSpinnerService} from 'ngx-spinner';
 import {GetDataService} from '../services/get-data.service';
 import {ActivatedRoute, Router, RouterLink} from "@angular/router";
 import { MatCard, MatCardActions, MatCardTitle } from '@angular/material/card';
@@ -20,222 +20,254 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatInput } from '@angular/material/input';
 import { catchError, map, merge, startWith, switchMap } from 'rxjs';
 import { ApiService } from '../api.service';
+import {NgClass} from "@angular/common";
 
 @Component({
-  selector: 'app-publications',
-  templateUrl: './publications.component.html',
-  styleUrls: ['./publications.component.css'],
-  standalone : true,
-  imports: [
-    MatCard,
-    MatCardTitle,
-    MatCardActions,
-    MatList,
-    MatListItem,
-    MatChipSet,
-    MatLine,
-    MatChip,
-    MatIcon,
-    MatProgressSpinner,
-    MatLabel,
-    MatFormField,
-    MatTable,
-    MatSort,
-    MatHeaderCellDef,
-    RouterLink,
-    MatHeaderCell,
-    MatCell,
-    MatAnchor,
-    MatColumnDef,
-    MatPaginator,
-    MatHeaderRow,
-    MatRow,
-    MatHeaderRowDef,
-    MatRowDef,
-    MatNoDataRow,
-    MatCellDef,
-    MatButton,
-    MatInput,
-    MatSortHeader,
-    MatProgressSpinner,
-    MatExpansionModule,
-    MatCheckboxModule
-]
+    selector: 'app-publications',
+    templateUrl: './publications.component.html',
+    styleUrls: ['./publications.component.css'],
+    standalone : true,
+    imports: [
+        MatCard,
+        MatCardTitle,
+        MatCardActions,
+        MatList,
+        MatListItem,
+        MatChipSet,
+        MatLine,
+        MatChip,
+        MatIcon,
+        MatProgressSpinner,
+        MatLabel,
+        MatFormField,
+        MatTable,
+        MatSort,
+        MatHeaderCellDef,
+        RouterLink,
+        MatHeaderCell,
+        MatCell,
+        MatAnchor,
+        MatColumnDef,
+        MatPaginator,
+        MatHeaderRow,
+        MatRow,
+        MatHeaderRowDef,
+        MatRowDef,
+        MatNoDataRow,
+        MatCellDef,
+        MatButton,
+        MatInput,
+        MatSortHeader,
+        MatProgressSpinner,
+        MatExpansionModule,
+        MatCheckboxModule,
+        NgClass,
+        NgxSpinnerModule
+    ]
 })
 export class PublicationsComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  dataSource = new MatTableDataSource<any>();
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort, { static: true }) sort: MatSort;
-  filterChanged = new EventEmitter<any>();
-  data: any;
-  dataCount = 0;
+    dataSource = new MatTableDataSource<any>();
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort, { static: true }) sort: MatSort;
+    filterChanged = new EventEmitter<any>();
+    data: any;
+    dataCount = 0;
+    searchValue: string;
+    pagesize = 20;
+    urlAppendFilterArray = []
+    activeFilters = new Array<string>();
+    columns = ['title', 'journal_name', 'year', 'organism_name', 'study_id'];
 
-  pagesize = 20;
-  urlAppendFilterArray = []
-  activeFilters = new Array<string>();
-  columns = ['title', 'journal_name', 'year', 'organism_name', 'study_id'];
-  journalNameFilters = [];
-  publicationYearFilters = [];
-  articleTypeFilters = [];
-  isLoadingResults: boolean;
-  isRateLimitReached: boolean;
-  aggregations: any;
-  resultsLength: any;
-  timer: any;
-  constructor(private titleService: Title,
-              private spinner: NgxSpinnerService,
-              private getDataService: GetDataService,
-              private activatedRoute: ActivatedRoute,
-              private router: Router) { }
+    journalFilters: any[] = [];
+    pubYearFilters: any[] = [];
+    articleTypeFilters: any[] = [];
+    queryParams: any = {};
 
-  ngOnInit(): void {
-    this.titleService.setTitle('Publications');
-    const queryParamMap = this.activatedRoute.snapshot.queryParamMap;
-    const params = queryParamMap['params'];
-    if (Object.keys(params).length !== 0) {
-      this.resetFilter();
-      for (const key in params) {
-        this.urlAppendFilterArray.push({name: key, value: params[key]});
-        this.activeFilters.push(params[key]);
-      }
-    }
-    // this.getAllPublications(0, this.pagesize, this.sort.active, this.sort.direction);
-  }
+    isLoadingResults: boolean;
+    isRateLimitReached: boolean;
+    aggregations: any;
+    resultsLength: any;
+    timer: any;
+    constructor(private titleService: Title,
+                private spinner: NgxSpinnerService,
+                private getDataService: GetDataService,
+                private activatedRoute: ActivatedRoute,
+                private router: Router) { }
 
-  ngAfterViewInit() {
-    // If the user changes the metadataSort order, reset back to the first page.
-    this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
-    this.filterChanged.subscribe(() => (this.paginator.pageIndex = 0));
-    merge(this.paginator.page, this.sort.sortChange,this.filterChanged)
-        .pipe(
-            startWith({}),
-            switchMap(() => {
-              this.isLoadingResults = true;
-              return  this.getDataService.getAllPublications(0, this.pagesize, this.activeFilters)
-              .pipe(catchError(() => observableOf(null)));
-            }),
-            map(data => {
-              // Flip flag to show that loading has finished.
-              this.isLoadingResults = false;
-              this.isRateLimitReached = data === null;
-
-              if (data === null) {
-                return [];
-              }
-              this.resultsLength = data.count;
-              this.aggregations = data.aggregations;
-    
-              
-              this.dataSource = data.results;
-              this.dataCount = data.count;
-              this.publicationYearFilters = data.aggregations.pubYear?.buckets;
-              console.log(this.publicationYearFilters)
-              this.journalNameFilters = data.aggregations.journalTitle?.buckets;
-              this.articleTypeFilters = data.aggregations.articleType?.buckets;
-
-              this.router.navigate([], {
-                relativeTo: this.activatedRoute, queryParams:this.activeFilters,queryParamsHandling: 'merge'
-              })
-              return data.results;
-            }),
-        )
-        .subscribe(data => (this.data = data));
-  }
-
-  // getAllPublications(offset, limit, sortColumn?, sortOrder?): void {
-  //   this.spinner.show();
-  //   this.getDataService.getAllPublications(offset, limit, this.activeFilters).subscribe(
-  //       data => {
-  //         this.dataSource = data.results;
-  //         this.dataCount = data.count;
-  //         this.publicationYearFilters = data.aggregations.pubYear?.buckets;
-  //         this.journalNameFilters = data.aggregations.journalTitle?.buckets;
-  //         this.articleTypeFilters = data.aggregations.articleType?.buckets;
-  //         this.spinner.hide();
-  //       }
-  //   );
-  // }
-
-  getJournalName(data: any): string {
-    if (data.journalTitle !== undefined) {
-      return data.journalTitle;
-    } else {
-      return 'Wellcome Open Res';
-    }
-  }
-
-  getYear(data: any): string {
-    if (data.pubYear !== undefined) {
-      return data.pubYear;
-    } else {
-      return '2022';
-    }
-  }
-
-  // pageChanged(event: any): void {
-  //   const offset = event.pageIndex * event.pageSize;
-  //   this.getAllPublications(offset, event.pageSize);
-  // }
-
-  customSort(event: any): void {
-    console.log(event);
-  }
-
-  hasActiveFilters(): boolean {
-    if (typeof this.activeFilters === 'undefined') {
-      return false;
-    }
-    for (const key of Object.keys(this.activeFilters)) {
-      if (this.activeFilters[key].length !== 0) {
-        return true;
-      }
-    }
-    return false;
-  }
-  checkFilterIsActive = (filter: string) => {
-    // console.log(this.filterService.activeFilters);
-    if (this.activeFilters.indexOf(filter) !== -1) {
-      return 'active-filter';
+    ngOnInit(): void {
+        this.titleService.setTitle('Publications');
+        const queryParamMap = this.activatedRoute.snapshot.queryParamMap;
+        const params = queryParamMap['params'];
+        if (Object.keys(params).length !== 0) {
+            this.resetFilter();
+            for (const key in params) {
+                this.urlAppendFilterArray.push({name: key, value: params[key]});
+                this.activeFilters.push(params[key]);
+            }
+        }
     }
 
-  }
-  onFilterClick(filterValue: string) {
-    clearTimeout(this.timer);
-    const index = this.activeFilters.indexOf(filterValue);
-    index !== -1 ? this.activeFilters.splice(index, 1) : this.activeFilters.push(filterValue);
-    this.filterChanged.emit();
-  }
+    ngAfterViewInit() {
+        // If the user changes the metadataSort order, reset back to the first page.
+        this.sort.sortChange.subscribe(() => (this.paginator.pageIndex = 0));
+        this.filterChanged.subscribe(() => (this.paginator.pageIndex = 0));
+        merge(this.paginator.page, this.sort.sortChange, this.filterChanged)
+            .pipe(
+                startWith({}),
+                switchMap(() => {
+                    this.isLoadingResults = true;
+                    return  this.getDataService.getPublicationsData(this.paginator.pageIndex,
+                        this.paginator.pageSize, this.searchValue, this.sort.active, this.sort.direction, this.activeFilters,
+                        'articles')
+                        .pipe(catchError(() => observableOf(null)));
+                }),
+                map(data => {
+                    // Flip flag to show that loading has finished.
+                    this.isLoadingResults = false;
+                    this.isRateLimitReached = data === null;
 
+                    if (data === null) {
+                        return [];
+                    }
+                    this.resultsLength = data.count;
+                    this.aggregations = data.aggregations;
 
-  removeFilter(): void {
-    this.resetFilter();
-    const currentUrl = this.router.url;
-    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
-      this.router.navigate([currentUrl.split('?')[0]] );
-      this.spinner.show();
-      setTimeout(() => {
-        this.spinner.hide();
-      }, 800);
-    });
-  }
+                    this.articleTypeFilters = [];
+                    if (this.aggregations.articleType.buckets.length > 0) {
+                        this.articleTypeFilters = this.merge(this.articleTypeFilters,
+                            this.aggregations.articleType.buckets,
+                            'article_type');
+                    }
 
-  resetFilter(): void {
-    for (const key of Object.keys(this.activeFilters)) {
-      this.activeFilters[key] = [];
+                    this.journalFilters = [];
+                    if (this.aggregations.journalTitle.buckets.length > 0) {
+                        this.journalFilters = this.merge(this.journalFilters,
+                            this.aggregations.journalTitle.buckets,
+                            'journal_title');
+                    }
+
+                    this.pubYearFilters = [];
+                    if (this.aggregations.pubYear.buckets.length > 0) {
+                        this.pubYearFilters = this.merge(this.pubYearFilters,
+                            this.aggregations.pubYear.buckets,
+                            'pub_year');
+                    }
+
+                    this.queryParams = [...this.activeFilters];
+
+                    // add search value to URL query param
+                    if (this.searchValue) {
+                        this.queryParams.push(`searchValue - ${this.searchValue}`);
+                    }
+
+                    this.replaceUrlQueryParams();
+
+                    this.dataSource = data.results;
+                    this.dataCount = data.count;
+                    return data.results;
+                }),
+            )
+            .subscribe(data => (this.data = data));
     }
-    this.activeFilters = [];
-    this.urlAppendFilterArray = [];
 
-  }
+    replaceUrlQueryParams() {
+        this.router.navigate([], {
+            relativeTo: this.activatedRoute,
+            queryParams: this.queryParams,
+            replaceUrl: true,
+            skipLocationChange: false
+        });
+    }
 
-  ngOnDestroy(): void {
-    this.resetFilter();
-  }
+    merge = (first: any[], second: any[], filterLabel: string) => {
+        for (let i = 0; i < second.length; i++) {
+            second[i].label = filterLabel;
+            first.push(second[i]);
+        }
+        return first;
+    }
+
+    getJournalName(data: any): string {
+        if (data.journalTitle !== undefined) {
+            return data.journalTitle;
+        } else {
+            return 'Wellcome Open Res';
+        }
+    }
+
+    getYear(data: any): string {
+        if (data.pubYear !== undefined) {
+            return data.pubYear;
+        } else {
+            return '2022';
+        }
+    }
+
+    // pageChanged(event: any): void {
+    //   const offset = event.pageIndex * event.pageSize;
+    //   this.getAllPublications(offset, event.pageSize);
+    // }
+
+    customSort(event: any): void {
+        console.log(event);
+    }
+
+    hasActiveFilters(): boolean {
+        if (typeof this.activeFilters === 'undefined') {
+            return false;
+        }
+        for (const key of Object.keys(this.activeFilters)) {
+            if (this.activeFilters[key].length !== 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+    checkFilterIsActive = (filter: string) => {
+        // console.log(this.filterService.activeFilters);
+        if (this.activeFilters.indexOf(filter) !== -1) {
+            return 'active-filter';
+        }
+
+    }
+
+    onFilterClick(filterValue: string) {
+        clearTimeout(this.timer);
+        const index = this.activeFilters.indexOf(filterValue);
+        index !== -1 ? this.activeFilters.splice(index, 1) : this.activeFilters.push(filterValue);
+        this.filterChanged.emit();
+    }
+
+
+    removeFilter(): void {
+        this.resetFilter();
+        const currentUrl = this.router.url;
+        this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
+            this.router.navigate([currentUrl.split('?')[0]] );
+            this.spinner.show();
+            setTimeout(() => {
+                this.spinner.hide();
+            }, 800);
+        });
+    }
+
+    resetFilter(): void {
+        for (const key of Object.keys(this.activeFilters)) {
+            this.activeFilters[key] = [];
+        }
+        this.activeFilters = [];
+        this.urlAppendFilterArray = [];
+
+    }
+
+    ngOnDestroy(): void {
+        this.resetFilter();
+    }
 
 }
 
 function observableOf(arg0: null): any {
-  throw new Error('Function not implemented.');
+    throw new Error('Function not implemented.');
 }
 
--- src/app/services/get-data.service.ts
+++ src/app/services/get-data.service.ts
@@ -1,32 +1,73 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetDataService {
 
-  private API_BASE_URL = 'https://portal.darwintreeoflife.org/statuses_update/';
+  // private API_BASE_URL = 'https://portal.darwintreeoflife.org/statuses_update/';
+  private API_BASE_URL = 'http://127.0.0.1:8000';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private router: Router,
+              private activatedRoute: ActivatedRoute) {
+  }
 
-  // @ts-ignore
-  getAllPublications(offset, limit, filter?) {
-    const filters = [];
-    let url = `${this.API_BASE_URL}articles?offset=${offset}&limit=${limit}`;
-    for (const key of filter) {
-      if (['Genome Note', 'Research Article'].indexOf(key) !== -1) {
-        filters.push(`articleType=${key}`);
-      } else if (['2020', '2021', '2022', '2023', '2024'].indexOf(key) !== -1) {
-        filters.push(`pubYear=${key}`);
-      } else {
-        filters.push(`journalTitle=${key}`);
+  getPublicationsData(pageIndex: number, pageSize: number, searchValue: string, sortActive: string, sortDirection: string,
+                      filterValue: string[], indexName: string) {
+
+    const sortActiveESField: { [index: string]: any } = {
+      title: 'title.keyword',
+      study_id: 'study_id.keyword',
+      organism_name: 'organism_name.keyword'
+    };
+
+    const offset = pageIndex * pageSize;
+    let url = `${this.API_BASE_URL}/${indexName}?limit=${pageSize}&offset=${offset}`;
+    if (searchValue) {
+      url += `&search=${searchValue}`;
+    }
+    if (sortActive && sortDirection) {
+      url += `&sort=${sortActive in sortActiveESField ? sortActiveESField[sortActive] : sortActive}:${sortDirection}`;
+    }
+    if (filterValue.length !== 0) {
+      let filterStr = '&filter=';
+      let filterItem;
+      for (let i = 0; i < filterValue.length; i++) {
+        const filterName = filterValue[i].split('-')[0];
+        const filterVal = filterValue[i].split('-')[1];
+        if (filterName === 'article_type') {
+          filterItem = `articleType:${filterVal}`;
+        } else if (filterName === 'pub_year') {
+          filterItem = `pubYear:${filterVal}`;
+        } else if (filterName === 'journal_title') {
+          filterItem = `journalTitle:${filterVal}`;
+        }
+        filterStr === '&filter=' ? filterStr += `${filterItem}` : filterStr += `,${filterItem}`;
       }
+      url += filterStr;
     }
-    for (const key of filters) {
-      url = `${url}&${key}`;
-    }
+
+
+    // will not reload the page, but will update query params
+    this.router.navigate([],
+        {
+          relativeTo: this.activatedRoute,
+          queryParams: {
+            filter: filterValue,
+            sortActive: sortActive in sortActiveESField ? sortActiveESField[sortActive] : sortActive,
+            sortDirection,
+            searchValue,
+            pageIndex,
+            pageSize
+          },
+          queryParamsHandling: 'merge',
+        });
+
     return this.http.get<any>(url);
   }
+
 }
